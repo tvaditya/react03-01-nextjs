9ca25dcbfb8eca81c63b24b1181372133ce771fc
tvaditya <tvaditya@gmail.com>
2021-06-30 00:41:50 -0300

    Post's prewview created

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -38,7 +38,7 @@ export const getServerSideProps: GetServerSideProps = async ({req, params}) => {
     const { slug } = params;
     console.log(session)
 
-    if(!session.activeSubscription) {
+    if(!session?.activeSubscription) {
         return {
             redirect: {
                 destination: '/',
